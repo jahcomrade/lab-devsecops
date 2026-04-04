e0c21d518167b1ae3c2998cea5e9ac294d32ae4e
Jorge H. <ingjahb@gmail.com>
2026-04-04 11:48:10 -0500

    Mi primer pipeline DevSecOps

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Esto es una vulnerabilidad de seguridad (Hardcoded Secret)
+        String dbPassword = "supersecretpassword123"; 
+        System.out.println("Iniciando aplicación...");
+    }
+}
