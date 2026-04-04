6e45aaab1209b55c267ed28edecabde626498498
Jorge H. <ingjahb@gmail.com>
2026-04-04 12:15:30 -0500

    Laboratorio táctico: OWASP, SCA y Leaks

--- main.java
+++ main.java
@@ -1,7 +1,14 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // Esto es una vulnerabilidad de seguridad (Hardcoded Secret)
-        String dbPassword = "supersecretpassword123"; 
-        System.out.println("Iniciando aplicación...");
+    public static void main(String[] args) throws Exception {
+        // 1. LEAKS: Una llave de AWS (Gitleaks lo detectará)
+        String secret = "AKIAIMNO7YFZ5EXAMPLE";
+        
+        // 2. OWASP A03:2021 (Inyección SQL): Concatenar entrada en un Query
+        String id = "101"; // Imagina que esto viene de un usuario
+        String query = "SELECT * FROM users WHERE id = " + id; 
+        
+        System.out.println("Ejecutando: " + query);
     }
 }
